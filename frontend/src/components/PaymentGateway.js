bb2d896d8e417aacbe38a5dfb76bce820f39a744
Elenaperd24 <elena24.perdomo@gmail.com>
2023-03-01 06:59:38 +1100

    swetalert successul

--- frontend/src/components/PaymentGateway.js
+++ frontend/src/components/PaymentGateway.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Swiper, SwiperSlide } from 'swiper/react';
+import swal from 'sweetalert'
 
 import { loadStripe } from '@stripe/stripe-js'
 import { Elements, CardElement, AddressElement, useStripe, useElements } from "@stripe/react-stripe-js"; // all camponents that envold it. It will contect to stripe
@@ -23,26 +23,36 @@ const CheckOutForm = () => {
         e.preventDefault()
         // console.log('handle')
 
-        const { error, paymentMethod } = await stripe.createPaymentMethod({
-            type: 'card',
-            card: elements.getElement(CardElement), //use element to CardelementComponent,
-
-        })
-
-        if (!error) {
-            console.log(paymentMethod)
-            const { id } = paymentMethod
-
-            const { data } = await axios.post('http://localhost:4000/api/checkout', {
-                id,
-                amount: 10000
+        try {
+            const { error, paymentMethod } = await stripe.createPaymentMethod({
+                type: 'card',
+                card: elements.getElement(CardElement), //use element to CardelementComponent,
+    
             })
-
-            console.log(data)
-
-        }
-        else {
-            alert(error.message)
+    
+            if (!error) {
+                console.log(paymentMethod)
+                const { id } = paymentMethod
+    
+                const { data } = await axios.post('http://localhost:4000/api/checkout', {
+                    id,
+                    amount: 20000
+                })
+    
+                console.log(data)
+                swal({
+                    title: "successul payment",
+                    icon: "success",
+                    buttons: "ok"
+                })
+    
+            }
+            else{
+                alert(error.message)
+            }
+    
+        } catch (error) {
+            console.log(error)
         }
 
     }
@@ -53,14 +63,21 @@ const CheckOutForm = () => {
             <form onSubmit={handleBuy} className="card card-body">
 
                 <img className="img-fluid" src="https://www.heart.org/-/media/Images/Healthy-Living/Fitness/BreakingDownBarriersFitness.jpg?h=417&w=740&hash=DC427ED98E1ED6D0D269A8131CF0AF89" alt="BreakingDownBarriers" ></img>
-                <h3 className="text-center">price: 100 AUD</h3>
-                <h3><input value={name}></input></h3>
+                <div className="col">
+                <h3 className="text-center">price: 100 AUD</h3> 
+                <h3 className="text-center">Cant: 2</h3>
+                </div>
+                
+                <h3><input className="name" value={name}></input></h3>
                 <h3><input value={lastName}></input></h3>
-                <input placeholder="adress"></input>
+                <input name="adress" placeholder="adress"></input>
                 <div className="form-control">
                     
                     <CardElement className="form-control" /> {/* validate correct dates card by Card Element */}
 
+                </div>
+                <div>
+                    <h1>Total Price: 200</h1>
                 </div>
 
                 <button type="submit" className="btn btn-success">
@@ -84,12 +101,7 @@ function Payment() {
                 <div className="container">
                     <div className="row">
                         <div className="col-md-4 offset-md-4">
-                            <CheckOutForm />
-                            <LinkRouter to="/" >
-                                <button className="btn btn-primary">
-                                    back
-                                </button>
-                            </LinkRouter>
+                            <CheckOutForm />                          
                         </div>
 
                     </div>
